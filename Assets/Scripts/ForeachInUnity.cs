4d9f5f2ff92b886d43ef51566d8655f59d37d0e9
Frank28 <frank28@gmail.com>
2015-12-15 14:55:52 +0800

    ForeachDemo for compiled in Unity; ForeachDll for compiled in MD/VS

---
+++ Assets/Scripts/ForeachInUnity.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ForeachInUnity : MonoBehaviour {
+
+	Dictionary<int, int> _testDict = new Dictionary<int, int>();
+	const string s_label = "_InUnity";
+
+	void Start()
+	{
+		for (int i = 0; i < 100; i++)
+		{
+			_testDict.Add(i, i * i);
+		}
+	}
+
+	int _placeHolderRes = 0;
+	public void TestWithForeach()
+	{
+		_placeHolderRes = 0;
+		Profiler.BeginSample ("ForeachCompiled" + s_label);
+		foreach (var t in _testDict) {
+			_placeHolderRes = t.Value;
+		}
+		Profiler.EndSample ();
+	}
+
+	public void TestWithWhile()
+	{
+		_placeHolderRes = 0;
+		Profiler.BeginSample ("WhileCompiled" + s_label);
+		var e = _testDict.GetEnumerator ();
+		while (e.MoveNext ()) {
+			_placeHolderRes = e.Current.Value;
+		}
+		Profiler.EndSample ();
+	}
+
+	public void Update()
+	{
+		TestWithForeach ();
+		TestWithWhile ();
+	}
+}
